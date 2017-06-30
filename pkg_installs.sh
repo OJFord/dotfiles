5bb5a43b912ae122fe62547aed64995b160afddb
Ollie Ford <me@ojford.com>
2017-06-30 18:19:06 +0100

    Switch from aura to pacaur

--- pkg_installs.sh
+++ pkg_installs.sh
@@ -1,39 +1,32 @@
 #!/bin/sh
 
-from_std() {
+install() {
     pkg="$1"
-    sudo aura --noconfirm -Sy "$pkg"
+    pacaur --noconfirm -Sy "$pkg"
 }
 
-from_aur() {
-    pkg="$1"
-    sudo aura --noconfirm -Ay "$pkg"
-}
+pacaur -Syu
 
-sudo aura -Syu
-sudo aura -Ayu
-
-from_std dex
-from_std git
-from_std gnupg
-from_std i3-wm
-from_std irssi
-from_std libu2f-host
-from_std python
-from_std rustup
-from_std shellcheck
-from_std stack
-from_std terraform
-from_std thefuck
-from_std vim
-from_std yubico-c
-from_std yubico-c-client
-from_std yubico-pam
-from_std yubico-personalization
-
-from_aur alacritty-git
-from_aur albert
-from_aur compton-git
-from_aur ttf-iosevka-ss05
-from_aur ttf-iosevka-term-ss05
-from_aur xonsh
+install alacritty-git
+install albert
+install compton-git
+install dex
+install git
+install gnupg
+install i3-wm
+install irssi
+install libu2f-host
+install python
+install rustup
+install shellcheck
+install stack
+install terraform
+install thefuck
+install ttf-iosevka-ss05
+install ttf-iosevka-term-ss05
+install vim
+install xonsh
+install yubico-c
+install yubico-c-client
+install yubico-pam
+install yubico-personalization
