d89fa6dcf5a1b8ce0b995b4ba421dc5b35050c80
Ollie Ford <me@ojford.com>
2017-06-30 02:35:41 +0100

    Script install for existing configured progs

--- pkg_installs.sh
+++ pkg_installs.sh
@@ -4,6 +4,14 @@ sudo aura -Syu
 sudo aura -Ayu
 
 sudo aura --noconfirm -Sy dex
+sudo aura --noconfirm -Sy git
+sudo aura --noconfirm -Sy gnupg
+sudo aura --noconfirm -Sy i3-wm
+sudo aura --noconfirm -Sy irssi
+sudo aura --noconfirm -Sy thefuck
+sudo aura --noconfirm -Sy vim
 
+sudo aura --noconfirm -Ay alacritty
 sudo aura --noconfirm -Ay albert
 sudo aura --noconfirm -Ay compton-git
+sudo aura --noconfirm -Ay xonsh
