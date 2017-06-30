48f2a3d23f6cdeaa1737ef4a85685b0d10b3584b
Ollie Ford <me@ojford.com>
2017-06-30 20:04:02 +0100

    Use customised iosevka font

---
+++ font/build.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+set -e
+
+iosevka_dir="$XDG_CONFIG_HOME/font/iosevka"
+
+generate_font() {
+    name="$1"
+    style="$2"
+    up_style="$3"
+    it_style="$4"
+    ob_style="$5"
+    font_dir="$iosevka_dir/dist/iosevka-$name/ttf"
+
+    make custom-config set="$name" \
+        design="$style" \
+        upright="$up_style" \
+        italic="$it_style" \
+        oblique="$ob_style" \
+        && make custom set="$name"
+
+    cd "$font_dir"
+    for fontf in *; do
+        sudo ln -sf "$font_dir/$fontf" "/usr/share/fonts/TTF/$fontf"
+    done
+}
+
+if [ ! -d "$iosevka_dir" ]; then
+    git clone --depth=1 https://github.com/be5invis/Iosevka/ "$iosevka_dir"
+    cd "$iosevka_dir"
+else
+    cd "$iosevka_dir"
+    git pull
+fi
+npm install
+
+style="\
+    sans \
+    v-a-doublestory \
+    v-g-opendoublestory \
+    v-m-shortleg \
+    v-t-standard \
+    v-q-taily \
+    v-zero-slashed \
+    v-tilde-low \
+    v-asterisk-low \
+    v-paragraph-high \
+    v-caret-high \
+    v-underscore-low \
+    v-at-short \
+    v-eszet-sulzbacher \
+    v-dollar-through \
+    v-numbersign-upright \
+"
+
+up_style="\
+    v-l-zshaped \
+    v-i-serifed \
+    v-brace-straight \
+"
+
+it_style="\
+    v-l-hooky \
+    v-i-hooky \
+    v-brace-curly \
+"
+
+ob_style="\
+    v-l-hooky \
+    v-i-serifed \
+    v-brace-curly \
+"
+
+generate_font "custom-with-ligs" "$style" "$up_style" "$it_style" "$ob_style"
+generate_font "custom-no-ligs" "term $style" "$up_style" "$it_style" "$ob_style"
--- pkg_installs.sh
+++ pkg_installs.sh
@@ -5,6 +5,14 @@ install() {
     pacaur --noconfirm -Sy "$pkg"
 }
 
+trust_install() {
+    pkg="$1"
+    gpghome=/tmp/pacaurgpg
+    mkdir -p "$gpghome"
+    echo 'keyserver-options auto-key-retrieve' > "$gpghome"
+    GNUPGHOME="$gpghome" pacaur --noconfirm -Sy "$pkg"
+}
+
 pacaur -Syu
 
 install alacritty-git
@@ -16,17 +24,19 @@ install gnupg
 install i3-wm
 install irssi
 install libu2f-host
+install npm
+install otfcc
 install python
 install rustup
 install shellcheck
 install stack
 install terraform
 install thefuck
-install ttf-iosevka-ss05
-install ttf-iosevka-term-ss05
 install vim
 install xonsh
 install yubico-c
 install yubico-c-client
 install yubico-pam
 install yubico-personalization
+
+trust_install ttfautohint
