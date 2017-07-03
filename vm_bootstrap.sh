a0dd6fc0172f7029eb23197da7e86ebfa71a1f67
Ollie Ford <dev@ojford.com>
2017-07-03 23:52:09 +0100

    Refactor pacaur installation

--- vm_bootstrap.sh
+++ vm_bootstrap.sh
@@ -8,21 +8,34 @@ sudo pacman -Syu
 mkdir -p "$tmp_dir"
 cd "$tmp_dir"
 
-sudo pacman -S binutils make gcc fakeroot expac yajl git --noconfirm --needed
+install_pacaur() {
+    deps="\
+        binutils \
+        expac \
+        fakeroot \
+        gcc \
+        git \
+        make \
+        yajl \
+    "
+    sudo pacman -S --needed --noconfirm --noedit "$deps"
 
-if [ ! -n "$(pacman -Qs cower)" ]; then
-    curl -o PKGBUILD \
-        "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower-git"
-    makepkg PKGBUILD --install --needed
-fi
+    if [ ! -n "$(pacman -Qs cower)" ]; then
+        curl -o PKGBUILD \
+            "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower-git"
+        makepkg PKGBUILD --install --needed
+    fi
 
-if [ ! -n "$(pacman -Qs pacaur)" ]; then
-    curl -o PKGBUILD \
-        "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur"
-    makepkg PKGBUILD --install --needed
-fi
+    if [ ! -n "$(pacman -Qs pacaur)" ]; then
+        curl -o PKGBUILD \
+            "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur"
+        makepkg PKGBUILD --install --needed
+    fi
 
-pacaur -Syu
+    pacaur -Syu
+}
+
+install_pacaur
 pacaur -Sy open-vm-tools open-vm-tools-dkms
 
 # Cleanup automatic dotfiles that either won't be used, or will move to XDG dir
