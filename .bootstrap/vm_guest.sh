87e49e13a93f92ab967b34d1df153fdc06378851
Ollie Ford <dev@ojford.com>
2017-07-06 22:41:02 +0100

    Automate VM guest time sync

--- .bootstrap/vm_guest.sh
+++ .bootstrap/vm_guest.sh
@@ -3,6 +3,16 @@ set -e
 
 install_vm_guest() {
     require open-vm-tools
+
+    vmware-toolbox-cmd timesync enable
+    sudo hwclock --hctosys --localtime
+    require chrony
+    sudo tee /etc/chrony.conf <<-EOF
+		pool pool.ntp.org iburst
+		driftfile /var/lib/chrony/drift
+		makestep 1 3
+		rtcsync
+	EOF
 }
 
 install_vm_shared_dir() {
