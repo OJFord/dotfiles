19cf6c0dc4cff8fba8dd037329ffc50b5513c3e1
Ollie Ford <dev@ojford.com>
2017-08-31 21:59:33 +0100

    Move environment variable setting to login .profile

--- .profile
+++ .profile
@@ -9,6 +9,20 @@ gpg_agent_socket="$(gpgconf --list-dirs agent-ssh-socket)"
 export SSH_AUTH_SOCK="$gpg_agent_socket"
 
 export SHELL="fish"
-export TERMINAL=alacritty
+export TERMINAL="alacritty"
+
+export LANG="en_GB.UTF-8"
+export LC_CTYPE="en_GB.UTF-8"
+
+export VIMDIR="$XDG_CONFIG_HOME/vim"
+export VIMRC="$VIMDIR/.vimrc"
+export EDITOR="vim -u $VIMRC"
+export VISUAL="vim -u $VIMRC"
+
+export CARGO_HOME="$XDG_CONFIG_HOME/cargo"
+
+export PATH="$HOME/go/bin:$PATH"
+export PATH="$CARGO_HOME/bin:$PATH"
+export PATH="/usr/share/git/diff-highlight:$PATH"
 
 exec startx "$XINITRC"
--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,5 @@
 fish_vi_key_bindings
 
-set -x LANG en_GB.UTF-8
-set -x LC_CTYPE en_GB.UTF-8
-
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x PATH $XDG_CONFIG_HOME/bin $PATH
-
-set -x XDG_RUNTIME_DIR /var/run
-
-
-set -x VIMDIR $XDG_CONFIG_HOME/vim
-set -x VIMRC $VIMDIR/.vimrc
-
-set -x EDITOR "vim -u $VIMRC"
-set -x VISUAL "vim -u $VIMRC"
-
 alias dc docker-compose
 alias gitl "git lol"
 alias gits "git status"
@@ -23,12 +8,6 @@ alias ll "ls -l"
 alias lla "ll -A"
 alias ping prettyping
 alias vim "vim -u $VIMRC"
-
-set -x PATH $HOME/go/bin $PATH
-set -x CARGO_HOME = $XDG_CONFIG_HOME/cargo
-set -x PATH $CARGO_HOME/bin $PATH
-
-set -x PATH /usr/share/git/diff-highlight $PATH
 
 gpg-connect-agent updatestartuptty /bye
 set -x --universal GPG_TTY (tty)
