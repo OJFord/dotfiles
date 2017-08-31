3fb46116dda292b7c1fdbe0807de2d4cbb126c16
Ollie Ford <dev@ojford.com>
2017-08-31 21:58:30 +0100

    Simplify temperamental gpg-agent invocation

--- .profile
+++ .profile
@@ -3,6 +3,11 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XINITRC="$XDG_CONFIG_HOME/X/.xinitrc"
 export XSERVERRC="$XDG_CONFIG_HOME/X/.xserverrc"
 
+export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
+gpg-connect-agent /bye
+gpg_agent_socket="$(gpgconf --list-dirs agent-ssh-socket)"
+export SSH_AUTH_SOCK="$gpg_agent_socket"
+
 export SHELL="fish"
 export TERMINAL=alacritty
 
--- fish/config.fish
+++ fish/config.fish
@@ -8,17 +8,6 @@ set -x PATH $XDG_CONFIG_HOME/bin $PATH
 
 set -x XDG_RUNTIME_DIR /var/run
 
-# Set SSH to use gpg-agent
-set -x GNUPGHOME $XDG_CONFIG_HOME/gnupg
-if set --query SSH_AGENT_PID
-    set --erase SSH_AGENT_PID
-end
-if not gpg-agent >/dev/null ^&1; or not set --query SSH_AUTH_SOCK
-    killall gpg-agent >/dev/null ^&1
-    set --universal SSH_AUTH_SOCK (gpg-agent --daemon | cut -d'=' -f2 | cut -d';' -f1) ^/dev/null
-    set --erase GPG_TTY
-    set --universal GPG_TTY (tty)
-end
 
 set -x VIMDIR $XDG_CONFIG_HOME/vim
 set -x VIMRC $VIMDIR/.vimrc
@@ -40,3 +29,6 @@ set -x CARGO_HOME = $XDG_CONFIG_HOME/cargo
 set -x PATH $CARGO_HOME/bin $PATH
 
 set -x PATH /usr/share/git/diff-highlight $PATH
+
+gpg-connect-agent updatestartuptty /bye
+set -x --universal GPG_TTY (tty)
