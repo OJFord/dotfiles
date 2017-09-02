d93376361827ff34183ca320cfb5292036c8fe8a
Ollie Ford <dev@ojford.com>
2017-09-02 18:55:10 +0100

    Add public key for iosevka-generate dependency

--- .bootstrap/fonts.sh
+++ .bootstrap/fonts.sh
@@ -2,9 +2,7 @@
 set -e
 
 install_fonts() {
-    #!FIXME: iosevka-generate depends on ttfautohint, for which PGP check will fail
-    trust_require ttfautohint
-
+    gpg --keyserver pgp.mit.edu --recv-keys C1A60EACE707FDA5
     require iosevka-generate
     iosevka-generate
 
