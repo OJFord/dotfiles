73872782571b6d1e0acf9db37c1f864ce11f6800
Ollie Ford <dev@ojford.com>
2018-07-21 12:00:49 +0100

    Fix symlinking work/other context fish funcs

--- fish/config.fish
+++ fish/config.fish
@@ -71,13 +71,13 @@ for config in (find $XDG_CONFIG_HOME -wholename '*.dotfiles/fish/config.fish')
 end
 
 for autoload_fn in (find $XDG_CONFIG_HOME -wholename '*.dotfiles/fish/functions/*.fish')
-    if not test -L $autoload_fn
+    if not test -L (echo "$autoload_fn" | sed -r 's|.*\.dotfiles/fish|'"$XDG_CONFIG_HOME"'/fish|')
         ln -s $autoload_fn $XDG_CONFIG_HOME/fish/functions
     end
 end
 
 for completion in (find $XDG_CONFIG_HOME -wholename '*.dotfiles/fish/completions/*.fish')
-    if not test -L $completion
+    if not test -L (echo "$completion" | sed -r 's|.*\.dotfiles/fish|'"$XDG_CONFIG_HOME"'/fish|')
         ln -s $completion $XDG_CONFIG_HOME/fish/completions
     end
 end
