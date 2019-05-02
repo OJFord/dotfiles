c91ac3cf29b71fe3c05306f4c5d6fb37516675da
Ollie Ford <dev@ojford.com>
2019-05-02 13:55:06 +0100

    Move env var setup to login
    Should resolve several issues where GUIs don't find correct env.

--- .profile
+++ .profile
@@ -1,9 +1,44 @@
 #!/bin/sh
+export SHELL="fish"
+export TERMINAL="alacritty"
+
+export LANG="en_GB.UTF-8"
+export LC_CTYPE="en_GB.UTF-8"
+
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
+if [ "$(uname -s)" = Darwin ]; then
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+    export PATH="/usr/local/opt/findutils/libexec/bin:$PATH"
+    export MANPATH="/usr/local/opt/findutils/share/man:$MANPATH"
+
+    export PATH="/usr/local/opt/git/share/git-core/contrib/diff-highlight:$PATH"
+else
+    export PATH="/usr/share/git/diff-highlight:$PATH"
+fi
+
+
+# Homes
 export XDG_CONFIG_HOME="$HOME/.config"
+export PATH="$XDG_CONFIG_HOME/bin:$PATH"
+
 export XINITRC="$XDG_CONFIG_HOME/X/.xinitrc"
 export XSERVERRC="$XDG_CONFIG_HOME/X/.xserverrc"
 
+export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 
+export VIMDIR="$XDG_CONFIG_HOME/vim"
+export VIMRC="$VIMDIR/.vimrc"
+export EDITOR="vim -u $VIMRC"
+
+export CARGO_HOME="$XDG_CONFIG_HOME/cargo"
+export PATH="$CARGO_HOME/bin:$PATH"
+
+export NODE_PATH="/usr/local/lib/node_modules"
+
+
+# Startup
 if [ "$(uname -s)" != Darwin ]; then
     exec startx "$XINITRC"
 fi
--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,5 @@
+bass source .profile
 fish_vi_key_bindings
-
-set -x PATH \
-    "/usr/local/bin" \
-    "/usr/local/sbin" \
-    "/usr/bin" \
-    "/usr/sbin" \
-    "/bin" \
-    "/sbin"
 
 alias dc "set -e DOCKER_HOST; docker-compose"
 alias dkr "set -e DOCKER_HOST; docker"
@@ -27,41 +20,6 @@ status --is-interactive; and source (pyenv init - | psub)
 pyenv global 3.7.3
 
 alias vim "vim -u $VIMRC --"
-
-
-set -x SHELL "fish"
-set -x TERMINAL "alacritty"
-
-set -x LANG "en_GB.UTF-8"
-set -x LC_CTYPE "en_GB.UTF-8"
-
-if not set -q XDG_CONFIG_HOME
-    set -x XDG_CONFIG_HOME "$HOME/.config"
-end
-set -x CARGO_HOME "$XDG_CONFIG_HOME/cargo"
-set -x GNUPGHOME "$XDG_CONFIG_HOME/gnupg"
-
-set -x VIMDIR "$XDG_CONFIG_HOME/vim"
-set -x VIMRC "$VIMDIR/.vimrc"
-set -x EDITOR "vim -u $VIMRC"
-
-set -x PATH "$XDG_CONFIG_HOME/bin" \
-    "$CARGO_HOME/bin" \
-    "$HOME/go/bin" \
-    $PATH
-
-set -x NODE_PATH "/usr/local/lib/node_modules"
-
-if test (uname -s) = "Darwin"
-    set -x PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
-    set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman" $MANPATH
-    set -x PATH "/usr/local/opt/findutils/libexec/bin" $PATH
-    set -x MANPATH "/usr/local/opt/findutils/share/man" $MANPATH
-
-    set -x PATH "/usr/local/opt/git/share/git-core/contrib/diff-highlight" $PATH
-else
-    set -x PATH "/usr/share/git/diff-highlight" $PATH
-end
 
 gpg-connect-agent reloadagent updatestartuptty /bye >/dev/null
 set -e GPG_TTY; set -x --universal GPG_TTY (tty)
