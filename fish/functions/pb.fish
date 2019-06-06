850cb4e413b6b15a4d6273e7e5183bd05ee756a0
Ollie Ford <dev@ojford.com>
2019-06-06 19:18:15 +0100

    Accept password name immediately if an exact match
    This commit changes to read the password NAME if a <name> is given at
    the command line and it is exactly NAME, which exists.

--- fish/functions/pb.fish
+++ fish/functions/pb.fish
@@ -1,3 +1,13 @@
 function pb
-    passbase read (passbase ls | fzf --no-preview --no-multi --query="$argv") | pbcopy
+    begin
+        set --local IFS
+        set list (passbase ls)
+    end
+    if test "$argv"; and test (echo "$list" | rg --count "^$argv\$") -eq 1
+        set read "$argv"
+    else
+        set read (echo "$list" | fzf --no-preview --no-multi --query="$argv")
+    end
+
+    passbase read "$read" | pbcopy
 end
