a68d344d22b407b194c50f3af9c8c9eb3c160b2f
Ollie Ford <dev@ojford.com>
2020-04-17 19:17:12 +0100

    Replace Albert launcher with rofi
    Wasn't working, and seems quite janky anyway.

--- albert/aconfmgr.sh
+++
@@ -1,1 +0,0 @@
-AddPackage albert
--- i3/aconfmgr.sh
+++ i3/aconfmgr.sh
@@ -3,10 +3,6 @@ AddPackage i3-wm
 # Status bar
 AddPackage --foreign polybar
 
-# Omni launcher
-AddPackage albert
-AddPackage picom
-
 # XDG autostart
 AddPackage dex
 
