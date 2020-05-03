9aa02524d6ef2054ebba9642c1dae9f381a326db
Oliver Ford <dev@ojford.com>
2020-05-03 15:11:18 +0100

    Add ibus for multi-lingualisation

---
+++ ibus/.profile
@@ -0,0 +1,1 @@
+dconf write /desktop/ibus/general/engines-order "['xkb:us::eng', 'm17n:hi:itrans']"
---
+++ ibus/aconfmgr.sh
@@ -0,0 +1,4 @@
+AddPackage ibus-m17n
+
+IgnorePath '/etc/dconf/db/ibus'
+IgnorePath '/usr/share/glib-2.0/schemas/gschemas.compiled'
