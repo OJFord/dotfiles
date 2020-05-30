b0afb8e563eb10f25180e8ca10b84662391a64f1
Oliver Ford <dev@ojford.com>
2020-05-30 22:15:25 +0100

    Configure git send-email

--- git/aconfmgr.sh
+++ git/aconfmgr.sh
@@ -1,4 +1,12 @@
 AddPackage git
+
+# interactive.singleKey
 AddPackage perl-term-readkey
 
+# sendemail
+AddPackage perl-authen-sasl
+AddPackage perl-mime-tools
+AddPackage perl-net-smtp-ssl
+
+# pager.diff
 AddPackage diff-so-fancy
