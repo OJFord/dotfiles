42c2595b9889994b768f51c0453a193d55c1f531
Oliver Ford <dev@ojford.com>
2020-06-19 19:00:07 +0100

    Switch to binary `gh` as failing to build

--- gh/aconfmgr.sh
+++ gh/aconfmgr.sh
@@ -1,8 +1,1 @@
-AddPackage --foreign github-cli
-IgnorePath /usr/share/bash-completion/completions/gh
-IgnorePath /usr/share/doc/github-cli/README.md
-IgnorePath /usr/share/fish/vendor_completions.d/gh.fish
-IgnorePath /usr/share/licenses/github-cli
-IgnorePath /usr/share/man/man1/gh*
-IgnorePath /usr/share/site-functions/_gh
-IgnorePath /usr/share/zsh/site-functions/_gh
+AddPackage --foreign github-cli-bin
