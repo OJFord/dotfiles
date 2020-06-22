5fa1d86e43b13c15a1a01d7bd628aecbd6944344
Oliver Ford <dev@ojford.com>
2020-06-22 16:03:02 +0100

    Encrypt home dir

---
+++ pam/aconfmgr.sh
@@ -0,0 +1,6 @@
+# Decrypt home dir from $HOME.cipher
+AddPackage gocryptfs
+AddPackage pam_mount
+CopyFile /etc/fuse.conf
+CopyFile /etc/pam.d/system-login
+CopyFile /etc/security/pam_mount.conf.xml
