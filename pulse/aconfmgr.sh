386af509c6ce070aaabe37a8c89f93b065e9b87e
Oliver Ford <dev@ojford.com>
2020-11-03 20:33:35 +0000

    Fix #7: media keys non-functional

--- pulse/aconfmgr.sh
+++ pulse/aconfmgr.sh
@@ -1,8 +1,11 @@
 AddPackage pulseaudio-alsa
 CreateLink /etc/systemd/user/sockets.target.wants/pulseaudio.socket /usr/lib/systemd/user/pulseaudio.socket
 
+# keybindings
 AddPackage pavucontrol
+AddPackage playerctl
 
+# bluetooth
 AddPackage pulseaudio-bluetooth
 AddPackage blueman
 AddPackage bluez
