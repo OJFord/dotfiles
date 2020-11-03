ae166928cf7b35cd469f2e88686469e94e3930f1
Oliver Ford <dev@ojford.com>
2020-11-03 22:08:11 +0000

    Fix sluggish fish startup
    cf. edc/bass#92

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,12 @@
-bass source "$HOME/.profile"
+bass source .profile
 set --unexport fish_greeting
 fish_vi_key_bindings
 
-for config in (find "$XDG_CONFIG_HOME/" -name 'config.fish')
-    if [ "$config" != (status -f) ]
-        source $config
-    end
-end
-
-for function_d in (find "$XDG_CONFIG_HOME/" -name functions -type d)
+for function_d in (find "$XDG_CONFIG_HOME/" -maxdepth 3 -name functions -type d)
     set fish_function_path "$function_d" $fish_function_path
 end
 
-for completion_d in (find "$XDG_CONFIG_HOME/" -name completions -type d)
+for completion_d in (find "$XDG_CONFIG_HOME/" -maxdepth 3 -name completions -type d)
     set fish_completion_path "$completion_d" $fish_completion_path
 end
 
