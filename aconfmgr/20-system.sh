fd2ecbf6471c3d7d03cbf6615aae7fe67e048568
Oliver Ford <dev@ojford.com>
2020-11-14 03:33:55 +0000

    Fix no command `hostname`

--- aconfmgr/20-system.sh
+++ aconfmgr/20-system.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=bash
-if [ "$(hostname)" = desk ]; then
+if [ "$(uname --nodename)" = desk ]; then
     AddPackage xf86-video-ati
     CopyFile /etc/X11/xorg.conf.d/10-benq-bl2420pt.conf
 fi
