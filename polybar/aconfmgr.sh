25325bc1ffadff904894693490014fb0e6827750
Oliver Ford <dev@ojford.com>
2020-11-14 18:44:04 +0000

    Switch emoji to JoyPixels, use as fallback

--- polybar/aconfmgr.sh
+++ polybar/aconfmgr.sh
@@ -2,9 +2,6 @@
 AddPackage --foreign polybar
 AddPackage psmisc
 
-# Emoji fallback
-AddPackage noto-fonts-emoji
-
 AddPackage --foreign polybar-spotify
 AddPackage --foreign pulseaudio-control
 AddPackage --foreign jq-git
