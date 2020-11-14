a69bc2b915b7b50597a1bea824eacf6b94fe94ea
Oliver Ford <dev@ojford.com>
2020-11-14 03:26:43 +0000

    Add WiFi support

--- aconfmgr/20-network.sh
+++ aconfmgr/20-network.sh
@@ -8,3 +8,9 @@ IgnorePath /etc/resolv.conf
 CopyFile /etc/ssh/sshd_config
 CreateLink /etc/systemd/system/multi-user.target.wants/sshd.service /usr/lib/systemd/system/sshd.service
 IgnorePath /etc/ssh/ssh_host_*_key*
+
+# wifi
+AddPackage iwd
+CopyFile /etc/iwd/main.conf
+CreateLink /etc/systemd/system/multi-user.target.wants/iwd.service /usr/lib/systemd/system/iwd.service
+CopyFile /etc/systemd/network/25-wlan.network
