8f52b80d943f1ecbfb177585196d34b2fd955c2d
Oliver Ford <dev@ojford.com>
2021-05-06 21:12:21 +0100

    Move docker aconf

--- aconfmgr/60-dev.sh
+++ aconfmgr/60-dev.sh
@@ -6,16 +6,7 @@ AddPackage postgresql
 IgnorePath /etc/dbeaver/bundles.info
 IgnorePath /usr/lib/jvm/*
 
-AddPackage docker
-CreateLink /etc/systemd/system/multi-user.target.wants/docker.service /usr/lib/systemd/system/docker.service
-IgnorePath /opt/containerd
-IgnorePath /etc/docker/key.json
-CopyFile /etc/docker/daemon.json
-CopyFile /etc/modprobe.d/docker.conf
-CopyFile /etc/systemd/system/docker.service.d/resourcecontrol.conf
-
 AddPackage dbeaver
-AddPackage docker-compose
 AddPackage fd
 AddPackage fzf
 
---
+++ docker/aconfmgr.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+AddPackage docker
+CreateLink /etc/systemd/system/multi-user.target.wants/docker.service /usr/lib/systemd/system/docker.service
+IgnorePath /opt/containerd
+IgnorePath /etc/docker/key.json
+CopyFile /etc/docker/daemon.json
+CopyFile /etc/modprobe.d/docker.conf
+CopyFile /etc/systemd/system/docker.service.d/resourcecontrol.conf
+
+AddPackage docker-compose
