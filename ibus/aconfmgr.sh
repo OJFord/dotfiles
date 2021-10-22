7b8c64eeb7deef63e6b88fd4942b2557f1a63024
Oliver Ford <dev@ojford.com>
2021-10-22 16:33:41 +0100

    Fix continual package add/rm diff

--- ibus/aconfmgr.sh
+++ ibus/aconfmgr.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 AddPackage ibus-typing-booster
-AddPackage hunspell-en_GB
+AddPackage hunspell-en_gb
 CreateLink '/usr/share/hunspell/hi_IN.aff' "$XDG_CONFIG_HOME/ibus/hunspell-hi/Hindi/hi_IN.aff"
 CreateLink '/usr/share/hunspell/hi_IN.dic' "$XDG_CONFIG_HOME/ibus/hunspell-hi/Hindi/hi_IN.dic"
 
