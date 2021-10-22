e365c4838102e120fc4f06d07a2942acef43eeda
Oliver Ford <dev@ojford.com>
2021-10-22 17:59:13 +0100

    Fix parse errors on ff start

--- mozilla/firefox/profile/user.js
+++ mozilla/firefox/profile/user.js
@@ -4,7 +4,7 @@ user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
 user_pref('ui.context_menus.after_mouseup', true);
 
 // Privacy
-user_pref('privacy.firstparty.isolate, true);
+user_pref('privacy.firstparty.isolate', true);
 
 // Scrolling
 // https://wiki.archlinux.org/index.php/Firefox/Tweaks#Jerky_or_choppy_scrolling
@@ -12,7 +12,7 @@ user_pref('mousewheel.min_line_scroll_amount', 40);
 user_pref('general.smoothScroll', false);
 user_pref('general.smoothScroll.pages', false);
 user_pref('image.mem.min_discard_timeout_ms', 2140000000);
-user_pref('image.mem.max_decoded_image_kb', 2**19);
+user_pref('image.mem.max_decoded_image_kb', 524288);
 
 // http://forums.mozillazine.org/viewtopic.php?p=14854175#p14854175
 Components.utils.import("resource://gre/modules/Services.jsm");
