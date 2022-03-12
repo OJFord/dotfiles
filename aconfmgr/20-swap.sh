d90e1dc453395b8e9da0786d9e95e31a565ff2bc
Oliver Ford <dev@ojford.com>
2022-03-12 16:27:38 +0000

    Fix error enabling hibernation before swap created

--- aconfmgr/20-swap.sh
+++ aconfmgr/20-swap.sh
@@ -5,12 +5,16 @@ CopyFile /etc/systemd/system/swapfile.swap
 CopyFile /etc/systemd/system/swapfile-create.service
 CreateLink /etc/systemd/system/multi-user.target.wants/swapfile.swap /etc/systemd/system/swapfile.swap
 
-swap_device="$(findmnt --noheadings --output=source --target=/swapfile)"
-swap_offset="$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')"
+# Before and during first `apply`, the swap device does not exist so cannot be used until the second.
+if swap_device="$(findmnt --noheadings --output=source --target=/swapfile)"; then
+    swap_offset="$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')"
 
-# Enable hibernation
-sed -E "s/(title .*)/\1 (resume)/;s@(options .*)@\1 resume=${swap_device} resume_offset=${swap_offset}@" /boot/loader/entries/arch.conf \
-   > "$(CreateFile /boot/loader/entries/arch-resume-hibernation.conf)"
-SetFileProperty /boot/loader/entries/arch-resume-hibernation.conf mode 755
-CopyFile /etc/systemd/logind.conf.d/sleep.conf
-CopyFile /usr/lib/systemd/system-sleep/log 755
+    # Enable hibernation
+    sed -E "s/(title .*)/\1 (resume)/;s@(options .*)@\1 resume=${swap_device} resume_offset=${swap_offset}@" /boot/loader/entries/arch.conf \
+        > "$(CreateFile /boot/loader/entries/arch-resume-hibernation.conf)"
+    SetFileProperty /boot/loader/entries/arch-resume-hibernation.conf mode 755
+    CopyFile /etc/systemd/logind.conf.d/sleep.conf
+    CopyFile /usr/lib/systemd/system-sleep/log 755
+else
+    >&2 echo 'Swap device not yet created, cannot enable hibernation. Apply again.'
+fi
