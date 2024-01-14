88868952f3e1111b7ebdf46c3500ce57f0e02252
Oliver Ford <dev@ojford.com>
2024-01-14 10:37:27 +0000

    Fix c-h c-k split window navigation not working

---
+++ nvim/lua/splits.lua
@@ -0,0 +1,11 @@
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+vim.o.winwidth = 80
+vim.o.winminwidth = 0
+vim.o.winminheight = 0
+
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>')
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>')
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
