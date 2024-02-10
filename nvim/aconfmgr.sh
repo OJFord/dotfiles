3bc9cc28bc549eb9386266719f5193c7118714cd
Oliver Ford <dev@ojford.com>
2024-02-10 15:03:57 +0000

    Add more language server sources

--- nvim/aconfmgr.sh
+++ nvim/aconfmgr.sh
@@ -3,10 +3,14 @@ AddPackage neovim
 
 AddPackage darkman
 AddPackage eslint_d
+AddPackage fish
+AddPackage just
+AddPackage shellcheck
 
 # Language servers
 AddPackage gopls
 AddPackage lua-language-server
+AddPackage marksman
 AddPackage --foreign nodejs-vls
 AddPackage python-lsp-server
 AddPackage python-lsp-black
@@ -15,5 +19,6 @@ AddPackage --foreign python-pylsp-mypy
 AddPackage svelte-language-server
 AddPackage --foreign sql-language-server
 AddPackage --foreign terraform-ls
+AddPackage texlab
 AddPackage typescript-language-server
 AddPackage typescript-svelte-plugin
