d49388a78c7ae1ac6a58823aa1251ab004990f99
Oliver Ford <dev@ojford.com>
2024-02-11 18:08:09 +0000

    Refactor lua modules to allow fn reuse

--- nvim/lua/folds.lua
+++ nvim/lua/folds.lua
@@ -1,27 +1,45 @@
-local augroup = vim.api.nvim_create_augroup("folds", { clear = true })
+local M = {}
+M.augroup = vim.api.nvim_create_augroup(..., {})
 
-if vim.o.fillchars ~= '' then
-    vim.o.fillchars = vim.o.fillchars .. ',fold: '
-else
-    vim.o.fillchars = 'fold: '
+local setup_autocmds = function()
+    vim.api.nvim_clear_autocmds({ group = M.augroup })
+
+    vim.api.nvim_create_autocmd({ "CursorHold", "BufWinEnter", "WinEnter" }, {
+        group = M.augroup,
+        callback = function()
+            vim.cmd("AutoOrigamiFoldColumn")
+        end
+    })
+
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        group = M.augroup,
+        callback = M.foldopen,
+    })
 end
 
-vim.o.foldmethod = 'syntax'
-vim.cmd [[set foldopen-=block]]
-vim.o.foldtext = "v:lua.custom_foldtext()"
-_G.custom_foldtext = function()
-    local size = vim.v.foldend - vim.v.foldstart + 1
-    return vim.fn.getline(vim.v.foldstart) .. ' 󰁂' .. size
+local setup_keymap = function()
+    vim.keymap.set('n', '<Space>', M.foldtoggle)
 end
 
-vim.api.nvim_create_autocmd({ "CursorHold", "BufWinEnter", "WinEnter" }, {
-    group = augroup,
-    callback = function()
-        vim.cmd("AutoOrigamiFoldColumn")
+local setup_opts = function()
+    if vim.o.fillchars ~= '' then
+        vim.o.fillchars = vim.o.fillchars .. ',fold: '
+    else
+        vim.o.fillchars = 'fold: '
     end
-})
 
-local foldopen = function()
+    vim.o.foldmethod = 'syntax'
+    vim.cmd [[set foldopen-=block]]
+    vim.o.foldtext = 'v:lua.require("folds").foldtext()'
+end
+
+function M.setup()
+    setup_autocmds()
+    setup_keymap()
+    setup_opts()
+end
+
+function M.foldopen()
     if vim.fn.foldclosed(vim.fn.line('.')) ~= -1 then
         vim.cmd('foldopen!')
     else
@@ -29,18 +47,38 @@ local foldopen = function()
     end
 end
 
-local foldtoggle = function()
+function M.foldtext()
+    local size = vim.v.foldend - vim.v.foldstart + 1
+    return vim.fn.getline(vim.v.foldstart) .. ' 󰁂' .. size
+end
+
+function M.foldtoggle()
     if vim.fn.foldlevel(vim.fn.line('.')) == 0 then
         return
     end
 
-    if foldopen() == -1 then
+    if M.foldopen() == -1 then
         vim.cmd('foldclose')
     end
 end
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    group = augroup,
-    callback = foldopen,
-})
-vim.keymap.set('n', '<Space>', foldtoggle)
+function M.get_open()
+    local open = {}
+    for line = 1, vim.fn.line('$') do
+        if vim.fn.foldlevel(line) > 0 and vim.fn.foldclosed(line) == -1 then
+            table.insert(open, line)
+        end
+    end
+    return open
+end
+
+function M.open_multi(lines)
+    for _, line in ipairs(lines) do
+        -- check still closed because it might have opened with previous line
+        if vim.fn.foldclosed(line) ~= -1 then
+            vim.cmd(tostring(line) .. 'foldopen')
+        end
+    end
+end
+
+return M
