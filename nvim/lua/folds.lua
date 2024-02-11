36c7a154b5bcf371f241e919f556afc4e5c2af0e
Oliver Ford <dev@ojford.com>
2024-02-11 16:32:43 +0000

    Migrate fold config to lua, fix slow toggle
    Slow toggle (or needing cursor move to be fast) caused by wait for a
    character following `<Space>` due to longer mappings. Fixed by removing
    those mappings, since I haven't been using them.

--- nvim/lua/folds.lua
+++ nvim/lua/folds.lua
@@ -1,11 +1,46 @@
+local augroup = vim.api.nvim_create_augroup("folds", { clear = true })
+
 if vim.o.fillchars ~= '' then
     vim.o.fillchars = vim.o.fillchars .. ',fold: '
 else
     vim.o.fillchars = 'fold: '
 end
 
+vim.o.foldmethod = 'syntax'
+vim.cmd [[set foldopen-=block]]
 vim.o.foldtext = "v:lua.custom_foldtext()"
 _G.custom_foldtext = function()
     local size = vim.v.foldend - vim.v.foldstart + 1
     return vim.fn.getline(vim.v.foldstart) .. ' 󰁂' .. size
 end
+
+vim.api.nvim_create_autocmd({ "CursorHold", "BufWinEnter", "WinEnter" }, {
+    group = augroup,
+    callback = function()
+        vim.cmd("AutoOrigamiFoldColumn")
+    end
+})
+
+local foldopen = function()
+    if vim.fn.foldclosed(vim.fn.line('.')) ~= -1 then
+        vim.cmd('foldopen!')
+    else
+        return -1
+    end
+end
+
+local foldtoggle = function()
+    if vim.fn.foldlevel(vim.fn.line('.')) == 0 then
+        return
+    end
+
+    if foldopen() == -1 then
+        vim.cmd('foldclose')
+    end
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = augroup,
+    callback = foldopen,
+})
+vim.keymap.set('n', '<Space>', foldtoggle)
