033e80eeb81216860537c242ab5395bd3b1d265e
Oliver Ford <dev@ojford.com>
2026-01-19 14:43:33 +0000

    Fix gui polkit auth, for fn from bg at all

--- sway/aconfmgr.sh
+++ sway/aconfmgr.sh
@@ -1,6 +1,9 @@
 # shellcheck shell=bash
 AddPackage sway
 AddPackage --foreign sway-systemd
+# https://github.com/alebastr/sway-systemd/issues/40
+sed -i -E 's@^(exec .+/session.sh)$@\1 --add-env=XDG_SESSION_ID@' "$(GetPackageOriginalFile sway-systemd /etc/sway/config.d/10-systemd-session.conf)"
+# https://aur.archlinux.org/packages/sway-systemd#comment-1051236
 SetFileProperty /etc/sway/config.d/95-system-keyboard-config.conf deleted y # clobbers `input:keyboard`
 AddPackage --foreign wev
 
