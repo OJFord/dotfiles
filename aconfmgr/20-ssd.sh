680d66658da1cd9edb67814b46d6a8bcc438afd7
Oliver Ford <dev@ojford.com>
2026-02-07 23:36:04 +0000

    Improve SSD detection robustness

--- aconfmgr/20-ssd.sh
+++ aconfmgr/20-ssd.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=bash
-if [ "$(cat "/sys/block/$(findmnt --noheadings --output=SOURCE --target=/boot | sed -E 's@/dev/(.*)p[0-9]+$@\1@')/queue/rotational")" = 0 ]; then
+if [ "$(lsblk --noheadings --output=ROTA "$(findmnt --noheadings --output=SOURCE --target=/boot)")" = 0 ]; then
     >&2 echo 'Boot disk seems to be an SSD, loading SSD conf'
 else
     exit 0
