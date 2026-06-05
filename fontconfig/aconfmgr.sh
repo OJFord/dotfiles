19caa326f76bd5d23774efcf127de921cf8fcd00
Oliver Ford <dev@ojford.com>
2026-06-05 20:39:11 +0100

    Fix fontconfig not taking effect

--- fontconfig/aconfmgr.sh
+++ fontconfig/aconfmgr.sh
@@ -3,3 +3,5 @@ AddPackage ttf-font-awesome
 AddPackage noto-fonts
 AddPackage noto-fonts-emoji
 AddPackage noto-fonts-extra
+
+CreateLink /etc/fonts/conf.d/50-user.conf /usr/share/fontconfig/conf.avail/50-user.conf
